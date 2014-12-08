bd6c87c14bda299ea1a0420ebcb63c28da5aeee6
Mary Taft <Mary.Taft97@gmail.com>
2014-12-08 13:58:38 -0500

    interim update

--- classwork/12-08-more_semaphores.c
+++ classwork/12-08-more_semaphores.c
@@ -16,4 +16,33 @@ semctl(descriptor, index, operation, buffer) <sys/types.h> <sys/ipc.h>
       GETVAL: return the value
    - buffer
 
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+unions (as opposed to structs) - all its members share the same memory
+
+struct foo{
+   int x; //size == 4 bytes
+   char y; //size == 1 byte
+}; //total size == 5 bytes (the size of the sum of its members)
+
+union bar{
+   int x; //size == 4 bytes
+   char y; //size == 1 byte
+}; //total size == 4 bytes (the size of its largest member)
+
+can be used to iterate through each byte of a multi-byte data type (e.g. an int) (note: unions aren't necessary to do this, but they make the process pretty elegant--see below)
+
+union trickster{
+   int x; //size == 4 bytes
+   char y[4]; //size == 4 bytes
+};
+
+union trickster test_union;
+test_union.x = 765;
+int i=0;
+while(i<4){
+   printf("%d\n",test_union.y[i]);
+   i++;
+}
+
  */
