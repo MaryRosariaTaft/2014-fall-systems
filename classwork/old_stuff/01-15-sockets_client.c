f56e40f56fb7d7384fc4cfce1be66f81b48ac90a
Mary Taft <Mary.Taft97@gmail.com>
2015-01-15 14:07:00 -0500

    update

--- classwork/old_stuff/01-15-sockets_client.c
+++ classwork/old_stuff/01-15-sockets_client.c
@@ -12,7 +12,13 @@ int main(int argc, char **argv){
   //create socket
   socket_id = socket(AF_INET, SOCK_STREAM, 0);
 
-  //
+  //bind to port/address
+  struct sockaddr_in sock;
+  sock.sin_family = AF_INET;
+  sock.sin_port = htons(24601); //port HAS TO BE the same as the server port
+  inet_aton("149.89.150.1", &(sock.sin_addr)); //translate ip address to binary and put it inside our sock struct
+  bind(socket_id, (struct sockaddr *)(&sock), sizeof(sock));
+
 
   return 0;
 
--- classwork/old_stuff/01-15-sockets_server.c
+++ classwork/old_stuff/01-15-sockets_server.c
@@ -7,14 +7,20 @@ char *process(char *s);
 
 int main(int argc, char **argv){
 
-  int socket_id;
+  int socket_id, socket_client;
   char buffer[256];
   int i, b;
 
   //create socket
   socket_id = socket(AF_INET, SOCK_STREAM, 0);
 
-  //
+  //bind to port/address
+  struct sockaddr_in listener;
+  listener.sin_family = AF_INET;
+  listener.sin_port = htons(24601); //because why not
+  listener.sin_addr.s_addr = INADDR_ANY;
+     //sin_addr is a struct in and of itself
+  bind(socket_id, (struct sockaddr *)(&listener), sizeof(listener));
 
   return 0;
 
